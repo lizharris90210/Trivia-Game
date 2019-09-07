032cb06aa1be0a26bd84eb5e30ca89527e1c45e0
Liz Harris <lizharris90210@gmail.com>
2019-09-07 15:27:01 -0400

    countdown timer working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,29 +3,23 @@ $(document).ready(function(){
     
  
 // click to start button, this and instructions disappears when clicked
-$("#startButton").click = function startButton(){
-    // starts questions
-    // starts timer for each question
+var time = 30;
+var seconds = document.getElementById('seconds');
+var counter = setInterval(countdown, 1000);
 
-// Timer count down
-function startTimer(duration, display) {
-    var timer = duration, seconds;
-    interval(function () {
-        seconds = interval(timer % 60, 10);
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-        display.textContent = seconds;
-
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
+function countdown() {
+    if (time == -1) {
+        clearTimeout(counter);
+        nextQuestion();
+    } else {
+        seconds.innerHTML = time + ' seconds remaining';
+        time--;
+    }
 }
 
-function countDown() {
-    var thirtySeconds = 30000,
-        display = $('#seconds');
-    startTimer(thirtySeconds, display);
-};}
+function nextQuestion() {
+    console.log(NEXT)
+}
 // One question per page at a time. When answered, show correct or wrong and move to next question.
 // When timer runs out, page automatically goes to next question unless out of questions, then to End Page with stats
     // variables
